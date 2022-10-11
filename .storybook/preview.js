b8274eec49e34a08dcb0bd7886992915fec65ca2
Amaury Gz <gzamaury@hotmail.com>
2022-10-11 21:43:35 +0000

    Fix: ModuleBuildError: Module build failed (from ./node_modules/storybook-addon-next/dist/images/next-image-loader-stub.js): TypeError: unsupported file type: undefined (file: undefined)
    cause:
    https://github.com/primefaces/primeicons/blob/5.0.0/primeicons.css
    https://github.com/storybookjs/storybook/blob/v6.5.12/lib/builder-webpack5/src/preview/base-webpack.config.ts
    https://github.com/RyanClementsHax/storybook-addon-next/blob/v1.3.1/src/images/webpack.ts
    
    fix: https://github.com/RyanClementsHax/storybook-addon-next/blob/v1.6.9/src/images/webpack.ts

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,7 +3,7 @@ import "../styles/globals.scss";
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeflex/primeflex.css";
-// import "primeicons/primeicons.css";
+import "primeicons/primeicons.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
