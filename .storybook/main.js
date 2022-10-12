fb885aa9396937bbf8ce9cef4dd5767df830e388
Amaury Gz <gzamaury@hotmail.com>
2022-10-12 00:57:58 +0000

    Fix: Deprecated --static-dir CLI flag https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#deprecated---static-dir-cli-flag

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,16 @@
 module.exports = {
   stories: [
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    'storybook-addon-next',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "storybook-addon-next",
   ],
-  framework: '@storybook/react',
+  framework: "@storybook/react",
   core: {
-    builder: 'webpack5',
+    builder: "webpack5",
   },
-}
+  staticDirs: ["../public"],
+};
